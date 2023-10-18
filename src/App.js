6b4423d29c3cc24e14f0f8c00c4d761195ea4ad7
Richard <richardlwhittington@gmail.com>
2023-10-17 23:03:23 -0500

    edit code

--- src/App.js
+++ src/App.js
@@ -1,71 +1,171 @@
-import {useState, useEffect} from 'react';
-import './App.css';
-import axios from 'axios';
-import Recipe from './components/Recipe';
+// import {useState, useEffect} from 'react';
+// import './App.css';
+// import axios from 'axios';
+// import Recipe from './components/Recipe';
 
- function App() {
-  const [recipes, setRecipes] = useState([]);
-  const [userSearch, setUserSearch] = useState('');
+//  function App() {
+//   const [recipes, setRecipes] = useState([]);
+//   const [userSearch, setUserSearch] = useState('');
+//   const [loading, setLoading] = useState(false);
+
+//   useEffect(() => {
+//     if (!userSearch) return;
+//     if (loading) return;
+//     setLoading(true);
+
+//   const fetchRecipes = async () => {
+//     const options = {
+//       method: 'GET',
+//       url: 'https://tasty.p.rapidapi.com/recipes/auto-complete',
+//       params: {
+//         prefix: userSearch, 
+//       },
+//       headers: {
+//         'X-RapidAPI-Key': process.env.X_RapidAPI_Key,
+//         'X-RapidAPI-Host': 'tasty.p.rapidapi.com',
+//       },
+//     };
+
+//     try {
+//       const response = await axios.request(options);
+//       setRecipes(response.data.results);
+//       console.log(response.data.results);
+//     } catch (error) {
+//       if (error.response) {
+//         if (error.response.status === 429) {
+//           console.error('Too many requests: Rate limit exceeded.');
+    
+//         } else {
+//           console.error(`Request failed with status code ${error.response.status}`);
+          
+//         }
+//       } else if (error.request) {
+//         console.error('No response received');
+       
+//       } else {
+//         console.error('An error occurred', error.message);
+//   }
+
+//   }};
+//   setTimeout(() => {
+//     fetchRecipes();
+//     setLoading(false);
+//   }, 3000); 
+// },
+//   fetchRecipes();
+// },[userSearch]);
+
+//   const handleSearch = () => {
+//     setUserSearch(userSearch); 
+//   };
 
 
-  useEffect(() => {
-  const fetchRecipes = async () => {
-    const options = {
-      method: 'GET',
-      url: 'https://tasty.p.rapidapi.com/recipes/auto-complete',
-      params: {
-        prefix: userSearch, 
-      },
-      headers: {
-        'X-RapidAPI-Key': process.env.X_RAPID_API_KEY,
-        'X-RapidAPI-Host': 'tasty.p.rapidapi.com',
-      },
-    };
-
-    try {
-      const response = await axios.request(options);
-      setRecipes(response.data.results);
-    } catch (error) {
-      if (error.response) {
-        console.error(`Request failed with status code ${error.response.status}`);
-      } else if (error.request) {
-        console.error('No response received');
-      } else {
-        console.error('An error occurred', error.message);
-    }
-  }
-
-  };
-
-  fetchRecipes();
-},[userSearch]);
-
-
-
-  return (
-    <div className="App">
-      <header className="App-header">
+//   return (
+//     <div className="App">
+//       <header className="App-header">
       
        
-      <h2>Recipes</h2>
-      <div>
-          <input
-            type="text"
-            placeholder="Search for recipes..."
-            value={userSearch}
-            onChange={(e) => setUserSearch(e.target.value)}
-          />
-          <button>Search</button>
-        </div>
-      {recipes.map((recipe) => (
-          <Recipe key={recipe.id} title={recipe.name} />
-        ))}
+//       <h2>Recipes</h2>
+//       <div>
+//           <input
+//             type="text"
+//             placeholder="Search for recipes..."
+//             value={userSearch}
+//             onChange={(e) => setUserSearch(e.target.value)}
+//           />
+//           <button onClick={handleSearch}>Search</button>
+//         </div>
+//         {loading ? <p>Loading...</p> : null}
+//       {recipes.map((recipe) => (
+//           <Recipe key={recipe.id} title={recipe.name} />
+//         ))}
       
       
-      </header>
-    </div>
-  );
+//       </header>
+//     </div>
+//   );
 
+      
+//     }
+//       export default App;
+
+      import { useState, useEffect } from 'react';
+      import './App.css';
+      import axios from 'axios';
+      import Recipe from './components/Recipe';
+      
+      function App() {
+        const [recipes, setRecipes] = useState([]);
+        const [userSearch, setUserSearch] = useState('');
+        const [loading, setLoading] = useState(false);
+      
+        const fetchRecipes = async () => {
+          const options = {
+            method: 'GET',
+            url: 'https://tasty.p.rapidapi.com/recipes/auto-complete',
+            params: {
+              prefix: userSearch,
+            },
+            headers: {
+              'X-RapidAPI-Key': process.env.X_RapidAPI_Key,
+              'X-RapidAPI-Host': 'tasty.p.rapidapi.com',
+            },
+          };
+      
+          try {
+            const response = await axios.request(options);
+            setRecipes(response.data.results);
+            console.log(response.data.results);
+          } catch (error) {
+            if (error.response) {
+              if (error.response.status === 429) {
+                console.error('Too many requests: Rate limit exceeded.');
+              } else {
+                console.error(`Request failed with status code ${error.response.status}`);
+              }
+            } else if (error.request) {
+              console.error('No response received');
+            } else {
+              console.error('An error occurred', error.message);
+            }
+          }
+        };
+      
+        useEffect(() => {
+          if (!userSearch) return;
+          if (loading) return;
+          setLoading(true);
+      
+          setTimeout(() => {
+            fetchRecipes();
+            setLoading(false);
+          }, 3000);
+        }, [userSearch, fetchRecipes]);
+      
+        const handleSearch = () => {
+          setUserSearch(userSearch);
+        };
+      
+        return (
+          <div className="App">
+            <header className="App-header">
+              <h2>Recipes</h2>
+              <div>
+                <input
+                  type="text"
+                  placeholder="Search for recipes..."
+                  value={userSearch}
+                  onChange={(e) => setUserSearch(e.target.value)}
+                />
+                <button onClick={handleSearch}>Search</button>
+              </div>
+              {loading ? <p>Loading...</p> : null}
+              {recipes.map((recipe) => (
+                <Recipe key={recipe.id} title={recipe.name} />
+              ))}
+            </header>
+          </div>
+        );
       }
-
+      
       export default App;
